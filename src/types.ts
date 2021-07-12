58cab7ebea1979dc3abca976ce5b3a07cc8b3cc0
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-12 10:18:43 -0700

    Add Slate types

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,26 @@ export interface ExampleRequest {
 	exampleName: string;
 	exampleBody: string;
 }
+
+// types related to the Slate Editor
+interface CustomText {
+	text : string,
+	bold? : boolean,
+	italic? : boolean,
+	underline? : boolean,
+	strikethrough? : boolean
+}
+
+interface ParagraphElement {
+	type : 'paragraph',
+	children : CustomText[]
+}
+
+interface CodeElement {
+	type : 'code',
+	children : CustomText[]
+}
+
+type CustomElement = ParagraphElement | CodeElement;
+
+export type Descendant = CustomElement | CustomText;
