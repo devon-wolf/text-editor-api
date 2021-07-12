204adc5e703139e0ab010ed7fc3f43f69fe863ee
Devon Wolfkiel <devonwolf@tutanota.com>
2021-07-12 10:07:43 -0700

    Clear out README, clear out unused CRUD class

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# typescript-node-sandbox
-
-Trying to figure out the smoothest, leanest TS setup for Node.
+# text-editor-api
--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,3 @@
-import { Request, Response } from 'express';
-
-export abstract class CRUDModel {
-	public abstract create(req : Request, res : Response) : void;
-	public abstract read(req : Request, res : Response) : void;
-	public abstract update(req : Request, res : Response) : void;
-	public abstract delete(req : Request, res : Response) : void;
-}
-
 export class ExpressError extends Error {
 	status : number | undefined;
 }
